c23ec458e8f5c2c5fef17a2bd26bd3a51ff5d3c7
Sudan <sudanshakya08@gmail.com>
2022-11-02 15:12:02 +0545

    added animation

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 // loading 
 
-var loader = document.getElementById("preloader")
+// var loader = document.getElementById("preloader")
 
-window.addEventListener("load",setTimeout(()=>{
-    loader.style.display = "none";
-},
-0000));
+// window.addEventListener("load",setTimeout(()=>{
+//     loader.style.display = "none";
+// },
+// 0000));
+
+// animation
+
+const animate1 = document.querySelectorAll('.animate');
+
+
+const options ={
+    root:null,
+    threshold:0.5,
+    rootmargin:'3000px',
+} ;
+
+const observer = new IntersectionObserver((entries,observer)=>{
+     entries.forEach((entry)=>{
+        if (entry.isIntersecting){
+        entry.target.classList.add("fadeup-animate");
+        }else{
+            return;
+        }
+},options)});
+
+
+animate1.forEach((animate)=>{
+    observer.observe(animate);
+});
+
